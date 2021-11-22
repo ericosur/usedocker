49f506fa502806a3f4f3706fa89b81218cc06908
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2021-11-22 17:08:41 +0800

    UPDATE start-battery-historian.sh

--- battery-historian/start-battery-historian.sh
+++ battery-historian/start-battery-historian.sh
@@ -2,5 +2,5 @@
 
 # then use browser to "localhost:5678"
 
-docker run -p 5678:9999 gcr.io/android-battery-historian/stable:3.0 --port 9999
+docker run -n battery-hist -p 5678:9999 gcr.io/android-battery-historian/stable:3.0 --port 9999
 
